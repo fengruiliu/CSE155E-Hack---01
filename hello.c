7be71a9d85b33325c3d8a4193c9004ff85559d96
chaofan1314 <42583284+chaofan1314@users.noreply.github.com>
2018-08-23 16:54:33 -0500

    hello
    hello

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Chaofan Song Fengrui Liu
+ * Date: 2018/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Chaofan Song Fengrui Liu Computre Science\n");
+
+  return 0;
+}
